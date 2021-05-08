ef88b11e21861878f0b2f8c2014c3db5543f1ed9
Vakha <gaparkhoev.v.l@sberbank.ru>
2021-05-08 21:26:19 +0300

    refactored

--- ml_project/entities/params.py
+++ ml_project/entities/params.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-from .data_params import DataParams
 from .feature_params import FeatureParams
 from .split_params import SplittingParams
 from .training_params import TrainingParams
@@ -8,7 +7,6 @@ from .training_params import TrainingParams
 
 @dataclass()
 class Params:
-    data_params: DataParams = DataParams()
     feature_params: FeatureParams = FeatureParams()
     splitting_params: SplittingParams = SplittingParams()
     training_params: TrainingParams = TrainingParams()
