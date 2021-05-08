5636d6d6f8d0cdf9564c12b20bd32365d61c7cda
Vakha <gaparkhoev.v.l@sberbank.ru>
2021-05-08 13:42:22 +0300

    added tests

---
+++ tests/test_eval_pipeline.py
@@ -0,0 +1,27 @@
+import os
+from typing import List
+from dacite import from_dict
+import pytest
+from omegaconf import OmegaConf, DictConfig
+
+from ml_project.eval import eval_model
+from ml_project.entities import EvalParams
+
+
+@pytest.fixture
+def eval_params() -> EvalParams:
+
+    params_dict = {
+        "data_path": "data/heart.csv",
+        "model_path": "model.pkl",
+        "extractor_path": "extractor.pkl",
+        "output_data_path": "predicts.csv"
+
+    }
+    return from_dict(data_class=EvalParams, data=params_dict)
+
+
+def test_eval_full_pipeline(eval_params: EvalParams):
+    eval_model(eval_params)
+    assert os.path.exists(eval_params.output_data_path)
+
